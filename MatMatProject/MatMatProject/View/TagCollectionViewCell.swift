3901b54cc9c407c89a1355293d5c87762657a64e
UnGwan <jkj8842@gmail.com>
2024-01-17 13:09:26 +0900

    기본 작업3

---
+++ MatMatProject/MatMatProject/View/TagCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CollectionViewCell.swift
+//  MatMatProject
+//
+//  Created by 정운관 on 1/17/24.
+//
+
+import UIKit
+
+class TagCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MatMatProject/MatMatProject/View/UserTableViewCell.swift
@@ -0,0 +1,19 @@
+
+// 세로 스크롤 게시물 ViewCell
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
