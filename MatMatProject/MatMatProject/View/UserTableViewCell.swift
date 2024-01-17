ff55f787087309f4e8bd25a91a027b4c87728906
seungpyo Hong <62321931+tv1039@users.noreply.github.com>
2024-01-17 15:10:42 +0900

    셀라벨 추가

--- MatMatProject/MatMatProject/View/UserTableViewCell.swift
+++ MatMatProject/MatMatProject/View/UserTableViewCell.swift
@@ -7,15 +7,94 @@ class UserTableViewCell: UITableViewCell {
     
     static let identifier = "UserTableViewCell"
     
+    let DataManager: DataManger = DataManger()
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+        
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        let user = DataManager.getUserData(index: 0)
+        
+        // 식당 이름
+        let storeLabel: UILabel = {
+            let storeLabel = UILabel()
+            storeLabel.text = user.food.name
+            return storeLabel
+        }()
+        
+        // 식당 주소
+        let addressLabel: UILabel = {
+            let addressLabel = UILabel()
+            addressLabel.text = user.food.address
+            return addressLabel
+        }()
+        
+        // 찜하기
+        let pinButton: UIButton = {
+            let pinButton = UIButton()
+            //            pinButton.setImage("", for: .normal)
+            return pinButton
+        }()
+        
+        // 이모지
+        let emojiLabel: UILabel = {
+            let emojiLabel = UILabel()
+            emojiLabel.text = user.emoji
+            return emojiLabel
+        }()
+        
+        // 별점
+        let starLabel: UILabel = {
+            let starLabel = UILabel()
+            starLabel.text = user.star
+            return starLabel
+        }()
+        
+        //ID
+        let iDLabel: UILabel = {
+            let iDLabel = UILabel()
+            iDLabel.text = user.id
+            return iDLabel
+        }()
+        
+        // 콘텐츠
+        let contentLabel: UILabel = {
+            let contentLabel = UILabel()
+            contentLabel.text = user.content
+            return contentLabel
+        }()
+        
+        
+        let verticalStackView: UIStackView = {
+            let stackView = UIStackView(arrangedSubviews: [storeLabel, addressLabel])
+            stackView.axis = .vertical
+            stackView.spacing = 10
+            stackView.translatesAutoresizingMaskIntoConstraints = false
+            return stackView
+        }()
+        
+        NSLayoutConstraint.activate([
+            verticalStackView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            verticalStackView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        
     }
-
+    
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        
     }
-
+    
 }
