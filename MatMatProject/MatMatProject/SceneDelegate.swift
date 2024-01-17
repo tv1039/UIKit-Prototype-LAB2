f7824abb28bd013f98fe68315257065b4dba053e
UnGwan <jkj8842@gmail.com>
2024-01-17 13:05:04 +0900

    기본 작업2

--- MatMatProject/MatMatProject/SceneDelegate.swift
+++ MatMatProject/MatMatProject/SceneDelegate.swift
@@ -14,7 +14,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        
+        let mainVC = UINavigationController(rootViewController: MainViewController())
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = mainVC
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
