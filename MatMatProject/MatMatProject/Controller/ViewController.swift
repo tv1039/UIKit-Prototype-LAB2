5e27fea2650f3ed8e82a4e1d3a32d1b4d9919094
UnGwan <jkj8842@gmail.com>
2024-01-18 13:06:48 +0900

    0118-1

--- MatMatProject/MatMatProject/Controller/ViewController.swift
+++ MatMatProject/MatMatProject/Controller/ViewController.swift
@@ -11,15 +11,35 @@ class ViewController: UIViewController {
     
     
     let tagName = Tag.allCases
+    let scrollView = UIScrollView()
+    let contentStackView = UIStackView()
+    let taglabel = UILabel()
+    let miniScrollView = UIScrollView()
+    let lineView = UIView()
+    let postLabel = UILabel()
+
+    let image1 = UIImage(named: "content")
+    var viewButtonArray : [UIButton] = []
+    
+    //버튼을 반복문으로 만들 때 함수를 반복문에 넣어주기 위한 배열
     var functionArray : [Selector] = [#selector(hydo),#selector(alchol),#selector(dating),#selector(something)]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        setupNaviBar()
         buildInterface()
     }
     
+    //인터페이스 생성 메소드를 모아놈
+    func buildInterface() {
+        setupNaviBar()
+        buildBasicInterface()
+        makeTag()
+        makePost()
+        setAutoLayOut()
+    }
+    
+    //네비게이션바 설정
     func setupNaviBar() {
         let titleLabel = UILabel()
         titleLabel.text = "MatMat"
@@ -35,55 +55,24 @@ class ViewController: UIViewController {
         navigationController?.navigationBar.scrollEdgeAppearance = appearance
     }
     
-    func buildInterface() {
-        
-        // 메인
-        let scrollView = UIScrollView()
+    //스크롤뷰와 내용을 담을 스택뷰 설정
+    func buildBasicInterface() {
         scrollView.backgroundColor = .systemBackground
         view.addSubview(scrollView)
-        
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
-        ])
-        
         scrollView.contentSize = CGSize(width: view.frame.width, height: 1500)
         
-        
-        let contentStackView = UIStackView()
         contentStackView.spacing = 10
         contentStackView.axis = .vertical
         scrollView.addSubview(contentStackView)
+    }
+    
+    func makeTag() {
+        // 1. 주제별 맛집 내용
+        taglabel.text = "  주제별 맛집"
+        taglabel.textColor = .systemYellow
+        taglabel.translatesAutoresizingMaskIntoConstraints = false
         
-        
-        contentStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        NSLayoutConstraint.activate([
-            contentStackView.topAnchor.constraint(equalTo: scrollView.topAnchor , constant: 10)
-        ])
-        
-        
-        
-        
-        // contentStackView 안쪽 내용들
-        
-        let label = UILabel()
-        label.text = "  주제별 맛집"
-        label.textColor = .systemYellow
-        label.translatesAutoresizingMaskIntoConstraints = false
-        
-        
-        
-        contentStackView.addArrangedSubview(label)
-        
-        
-        // contentStackView.setCustomSpacing(10, after: view)
-        
-        let miniScrollView = UIScrollView()
+        contentStackView.addArrangedSubview(taglabel)
         miniScrollView.contentSize = CGSize(width: view.frame.width, height: 110)
         miniScrollView.frame = CGRect(x: 0, y: 0, width:300, height: 110)
         
@@ -103,7 +92,6 @@ class ViewController: UIViewController {
             imageView.frame = CGRect(x: xOffset, y: 0, width:100, height: 100)
             miniScrollView.addSubview(imageView)
             
-            
             let button = UIButton()
             button.alpha = 0.35
             button.frame = CGRect(x: xOffset, y: 0, width:100, height: 100)
@@ -111,10 +99,55 @@ class ViewController: UIViewController {
             miniScrollView.addSubview(button)
         }
         
-        
-        
-        
         miniScrollView.contentSize = CGSize(width: CGFloat(numberOfItems) * (itemWidth + spacing ) - spacing , height: itemHeight)
+        contentStackView.addArrangedSubview(miniScrollView)
+        // 2. 태그별 맛집과 피드 게시물을 구분하는 얇은 선
+        
+        lineView.backgroundColor = .systemGray
+        contentStackView.addArrangedSubview(lineView)
+    }
+    
+    // 게시물내용 (추후에 버튼이 아니라 게시물 형식으로 수정하면 좋음)
+    func makePost() {
+        
+        postLabel.text = "  나의 피드"
+        postLabel.textColor = .systemBrown
+        contentStackView.addArrangedSubview(postLabel)
+        
+        // 추후에 모델에서 갯수를 받아와서 생성
+        let numberOfButton = 3
+        for i in 0..<numberOfButton {
+            let button = UIButton()
+            
+            
+            button.setImage(image1, for: .normal)
+            contentStackView.addArrangedSubview(button)
+            button.addTarget(self, action: #selector(navigatePostFood), for: .touchUpInside)
+            if viewButtonArray.isEmpty {
+                contentStackView.setCustomSpacing(20, after: miniScrollView)
+            } else {
+                contentStackView.setCustomSpacing(20, after: viewButtonArray[i-1])
+            }
+            viewButtonArray.append(button)
+        }
+    }
+    
+    //오토 레이아웃 설정
+    func setAutoLayOut() {
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
+        
+        contentStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentStackView.topAnchor.constraint(equalTo: scrollView.topAnchor , constant: 10)
+        ])
+        
         miniScrollView.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
@@ -123,57 +156,11 @@ class ViewController: UIViewController {
             
         ])
         
-        contentStackView.addArrangedSubview(miniScrollView)
-        
-        
-        // 태그별 맛집과 피드 게시물을 구분하는 얇은 선
-        let lineView = UIView()
-        lineView.backgroundColor = .systemGray
-        contentStackView.addArrangedSubview(lineView)
-        
         lineView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             lineView.heightAnchor.constraint(equalToConstant: 2),
             lineView.topAnchor.constraint(equalTo: miniScrollView.bottomAnchor , constant: -6)
         ])
-        
-        
-        // 피드 게시물
-        
-        let viewButton1 = UIButton()
-        let label2 = UILabel()
-        label2.text = "  나의 피드"
-        label2.textColor = .systemBrown
-        contentStackView.addArrangedSubview(label2)
-        
-        let image1 = UIImage(named: "content")
-        viewButton1.setImage(image1, for: .normal)
-        contentStackView.addArrangedSubview(viewButton1)
-        
-        viewButton1.addTarget(self, action: #selector(navigate), for: .touchUpInside)
-        
-        contentStackView.setCustomSpacing(20, after: miniScrollView)
-        //뷰 이동 버튼 2
-        let viewButton2 = UIButton()
-        
-        let image2 = UIImage(named: "content")
-        viewButton2.setImage(image2, for: .normal)
-        contentStackView.addArrangedSubview(viewButton2)
-        
-        contentStackView.setCustomSpacing(20, after: viewButton1)
-        
-        //뷰 이동 버튼 3
-        let viewButton3 = UIButton()
-        
-        let image3 = UIImage(named: "content")
-        viewButton3.setImage(image3, for: .normal)
-        contentStackView.addArrangedSubview(viewButton3)
-        
-        viewButton3.addTarget(self, action: #selector(navigate), for: .touchUpInside)
-        
-        contentStackView.setCustomSpacing(20, after: viewButton2)
-        
-        
     }
     
     
@@ -185,29 +172,27 @@ extension ViewController {
     
     //가로 뷰
     @objc func hydo() {
-        getImageData(idx: 0)
+        navigateTagFood(idx: 0)
     }
     @objc func alchol() {
-        getImageData(idx: 1)
+        navigateTagFood(idx: 1)
     }
     @objc func dating() {
-        getImageData(idx: 2)
+        navigateTagFood(idx: 2)
     }
     @objc func something() {
-        getImageData(idx: 3)
+        navigateTagFood(idx: 3)
     }
     
     
-    func getImageData(idx : Int) {
+    func navigateTagFood(idx : Int) {
         let nextViewController = MapViewController()
         nextViewController.filter = Tag.allCases[idx]
         self.navigationController?.pushViewController(nextViewController, animated: true)
     }
     
-    
-    
     //세로 뷰
-    @objc func navigate() {
+    @objc func navigatePostFood() {
         let nextViewController = WebViewController()
         self.navigationController?.pushViewController(nextViewController, animated: true)
     }
