8175c08b33bba6ccc055e95d1e23c58294a1070d
UnGwan <jkj8842@gmail.com>
2024-01-19 09:17:40 +0900

    0119

---
+++ MatMatProject/MatMatProject/View/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  detailView.swift
+//  MatMatProject
+//
+//  Created by 정운관 on 1/18/24.
+//
+
+import UIKit
+
+class detailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
